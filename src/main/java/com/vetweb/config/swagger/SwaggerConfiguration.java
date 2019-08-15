fdca87cb24ac916f828633a258b4875679302c2e
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-08-15 17:28:04 -0300

    Documentation Swagger

--- src/main/java/com/vetweb/config/swagger/SwaggerConfiguration.java
+++ src/main/java/com/vetweb/config/swagger/SwaggerConfiguration.java
@@ -1,16 +1,28 @@
 package com.vetweb.config.swagger;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.vetweb.controllers.auth.LoginForm;
+import com.vetweb.controllers.auth.TokenDTO;
+import com.vetweb.entities.Clinic;
+import com.vetweb.entities.Role;
 import com.vetweb.entities.User;
+import com.vetweb.models.dto.ClinicCreateDTO;
+import com.vetweb.models.dto.UserCreateDTO;
+import com.vetweb.models.form.ClinicCreateForm;
+import com.vetweb.models.form.UserCreateForm;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -21,10 +33,10 @@ public class SwaggerConfiguration {
 	public Docket forumApi() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.vetweb"))
-				.paths(PathSelectors.ant("/**"))
+				.apis(RequestHandlerSelectors.basePackage("com.vetweb.controllers"))
+				.paths(PathSelectors.any())
 				.build()
-				.ignoredParameterTypes(User.class)
+				.ignoredParameterTypes(disableTemplateClassesModels())
 				
 				// Setting global parameter
 				.globalOperationParameters(Arrays.asList(
@@ -34,7 +46,43 @@ public class SwaggerConfiguration {
 						.modelRef(new ModelRef("string"))//Type parameter
 						.parameterType("header")
 						.required(false)
-						.build()));
+						.build()))
+						.apiInfo(apiInfo());
+	}
+	
+	
+	private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+        	.title("Vetweb - API")
+            .description("This is a vetweb product API being developed.")
+            .contact(new Contact("Murillo Pezzuol", "www.murillopezzuol.com", "murillopezzuol@hotmail.com"))
+            .license("License vetweb-api 1.0")
+            .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+            .version("1.0.0")
+            .build();
+    }
+	
+	
+	//Method that returns templates that will be hidden in the API documentation
+	private Class[] disableTemplateClassesModels(){
+		ArrayList<Class> modelsForDisable = new ArrayList<Class>();
+		
+		//Entities
+		modelsForDisable.add(Clinic.class);
+		modelsForDisable.add(Role.class);
+		modelsForDisable.add(User.class);
+		
+		//Models DTO
+		//modelsForDisable.add(TokenDTO.class);//Token
+		//modelsForDisable.add(ClinicCreateDTO.class);
+		//modelsForDisable.add(UserCreateDTO.class);
+		
+		//Models Form
+		//modelsForDisable.add(LoginForm.class);//LoginForm
+		//modelsForDisable.add(ClinicCreateForm.class);
+		//modelsForDisable.add(UserCreateForm.class);
+		
+		return modelsForDisable.toArray(new Class[modelsForDisable.size()]);
 	}
 	
 	
--- src/main/java/com/vetweb/controllers/UserController.java
+++ src/main/java/com/vetweb/controllers/UserController.java
@@ -20,6 +20,12 @@ import com.vetweb.models.form.UserCreateForm;
 import com.vetweb.services.IClinicService;
 import com.vetweb.services.IUserService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(tags="User Controller", description="Controller Description and....")
 @RestController
 @RequestMapping("/v1/user")
 public class UserController {
@@ -31,23 +37,31 @@ public class UserController {
 	private IClinicService clinicService;
 	
 	
+	@ApiOperation(value = "Find all Users.")
 	@GetMapping()
 	public ResponseEntity<List<User>> findAll() {
 		return ResponseEntity.ok(this.userService.findAll());
 	}
 	
 	
+	//Docs for Swagger
+	@ApiOperation(
+			value = "Create User passing the Clinic ID", 
+			response = UserCreateDTO.class,
+			notes="This operation creates a new clinical user by passing the clinic ID.")
+	@ApiResponses(value = {
+	    @ApiResponse(code = 200, message = "Returns a UserCreateDTO with a success message.", response=UserCreateDTO.class),
+	    @ApiResponse(code = 201, message = "Return message if status code is 201"),
+	    @ApiResponse(code = 401, message = "Return message if status code is 401"),
+	    @ApiResponse(code = 403, message = "Return message if status code is 403"),
+	    @ApiResponse(code = 404, message = "Return message if status code is 404")
+	})
 	@PostMapping()
 	public ResponseEntity<UserCreateDTO> createUser(@RequestBody @Valid UserCreateForm userCreateForm) {
-		
 		User user = userCreateForm.converterToUser();
-		
 		Optional<Clinic> optional = this.clinicService.findById(userCreateForm.getIdClinic());
-		
 		user.setClinic(optional.get());
-		
 		this.userService.save(user);
-		
 		return ResponseEntity.ok(new UserCreateDTO(user));
 	}
 	
